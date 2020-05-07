3f041a71b0a930a5246a9d4ce33bd71d37320f36
Spozder <spencer.pozder@gmail.com>
2020-05-07 12:46:03 -0400

    Initial commit - working vue webapp and api route

---
+++ srv/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+const history = require("connect-history-api-fallback");
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/../dist"));
+
+app.get("/api/hello", (req, res) => res.send("Hello World!"));
+
+app.use(
+  history({
+    index: "/index.html",
+    verbose: true,
+  })
+);
+
+// listen on the port
+console.log("Starting express app!");
+app.listen(port);
