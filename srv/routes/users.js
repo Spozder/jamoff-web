4404c746c02f3a9d81d3d6352fad965f43c386a2
Spencer Pozder <spozder@panoramaed.com>
2020-05-23 15:01:17 -0400

    Fix users routes, add bcrypt dep

--- srv/routes/users.js
+++ srv/routes/users.js
@@ -2,56 +2,37 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = eventDriver => {
-  const getNextUserId = callback => {
-    return eventDriver.getState((err, state) => {
-      return callback(
-        err,
-        String(Math.max(...Object.keys(state.profiles), 0) + 1)
-      );
-    });
-  };
-
-  const getNextIdentityId = callback => {
-    return eventDriver.getState((err, state) => {
-      return callback(
-        err,
-        String(Math.max(...Object.keys(state.identities), 0) + 1)
-      );
-    });
-  };
+  const { getsReadState, handleAppendEventError } = require("./middleware")(
+    eventDriver
+  );
 
   const getPassHash = passwd => {
     // TODO
     return passwd;
   };
 
-  router.get("/", (req, res) => {
-    return eventDriver.getState((err, state) => res.send(state.profiles));
+  router.get("/", getsReadState, (req, res) => {
+    return res.send(req.readState.profiles);
   });
 
   router.post("/", (req, res) => {
-    return getNextUserId((err, newUserId) => {
-      return getNextIdentityId((err, newIdentityId) => {
-        return eventDriver.appendEventData(
-          "EVENT:USER:USER_CREATED",
-          {
-            userId: newUserId,
-            identityId: newIdentityId,
-            email: req.body.email,
-            passHash: getPassHash(req.body.passwd),
-            ...(req.body.fullName && { fullName: req.body.fullName })
-          },
-          err => {
-            console.log("CREATE USER ERROR: ", err);
-            if (err) {
-              res.status(400).send("Invalid CREATE_USER body");
-            } else {
-              res.send({ id: newUserId });
-            }
-          }
-        );
-      });
-    });
+    const newUserId = req.readState.getNextProfileId();
+    const newIdentityId = req.readState.getNextIdentityId();
+    return eventDriver.appendEventData(
+      "EVENT:USER:USER_CREATED",
+      {
+        userId: newUserId,
+        identityId: newIdentityId,
+        email: req.body.email,
+        passHash: getPassHash(req.body.passwd),
+        ...(req.body.fullName && { fullName: req.body.fullName })
+      },
+      err => {
+        return handleAppendEventError(err, res, () => {
+          return res.send({ id: newUserId });
+        });
+      }
+    );
   });
 
   router.put("/", (req, res) => {
@@ -80,12 +61,9 @@ module.exports = eventDriver => {
         }
       ),
       err => {
-        if (err) {
-          console.log(err);
-          res.status(400).send("Invalid UPDATE_USER body");
-        } else {
-          res.sendStatus(200);
-        }
+        return handleAppendEventError(err, res, () => {
+          return res.sendStatus(200);
+        });
       }
     );
   });
