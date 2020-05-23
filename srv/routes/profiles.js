14ea20c8a3eb9c5067f0701db0ec23a9c51be5ff
Spencer Pozder <spozder@panoramaed.com>
2020-05-23 14:28:06 -0400

    Fixes profiles routes

--- srv/routes/profiles.js
+++ srv/routes/profiles.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { StateUtils } = require("../state");
 
 module.exports = eventDriver => {
-  router.get("/:id", (req, res) => {
-    return eventDriver.getState((err, state) => {
-      if (err) {
-        console.log("UH OH", err);
-        return res.sendStatus(500);
-      }
-      return res.send(StateUtils.getDisplayProfile(state, req.params["id"]));
-    });
+  const { getsReadState } = require("./middleware")(eventDriver);
+
+  router.get("/:id", getsReadState, (req, res) => {
+    return res.send(req.readState.getDetailedProfile(req.params["id"]));
   });
 
   return router;
